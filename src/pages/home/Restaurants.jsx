f9fa15229776cf3695fd59168362eff8eed69737
ramanboff <bayramofframan0@gmail.com>
2024-03-29 18:16:37 +0400

    Data fetching from json file

--- src/pages/home/Restaurants.jsx
+++ src/pages/home/Restaurants.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "../../components/card/Card";
 
 function Restaurants() {
+  // states
+  const [restaurants, setRestaurants] = useState(null);
+  //data fetching
+  useEffect(() => {
+    fetch("http://localhost:8000/restaurants")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setRestaurants(data);
+      });
+  }, []);
   return (
     <>
       <input
         className={Restaurants.searchInput}
-        onChange={handleSearchRestaurant}
         type="text"
         placeholder="search restaurant"
       />
+      <div className={Restaurants.cardsWrapper}>
+        {restaurants &&
+          restaurants.map((restaurant) => (
+            <Card key={restaurant.id} restaurant={restaurant} />
+          ))}
+      </div>
     </>
   );
 }
