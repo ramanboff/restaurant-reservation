82c714503f839755431862fe86ba2a13576811df
ramanboff <bayramofframan0@gmail.com>
2024-03-29 18:37:08 +0400

    added searching restaurant  functionallity

--- src/pages/home/Restaurants.jsx
+++ src/pages/home/Restaurants.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Card from "../../components/card/Card";
-import RestaurantsStyle from "./Restaurants.module.css"
+import RestaurantsStyle from "./Restaurants.module.css";
 
 function Restaurants() {
   // states
   const [restaurants, setRestaurants] = useState(null);
+  const [searchRestaurant, setSearchRestaurant] = useState("");
   //data fetching
   useEffect(() => {
     fetch("http://localhost:8000/restaurants")
@@ -16,16 +17,30 @@ function Restaurants() {
         setRestaurants(data);
       });
   }, []);
+
+  //search restaurant by name start
+  const handleSearchRestaurant = (e) => {
+    setSearchRestaurant(e.target.value.trim());
+  };
+
+  const filteredRestaurants = restaurants
+    ? restaurants.filter((restaurant) =>
+        restaurant.name.toLowerCase().includes(searchRestaurant.toLowerCase())
+      )
+    : [];
+  //search restaurant by name end
   return (
     <>
+    <h2>Restaurant Reservation</h2>
       <input
+        onChange={handleSearchRestaurant}
         className={RestaurantsStyle.searchInput}
         type="text"
         placeholder="search restaurant"
       />
       <div className={RestaurantsStyle.cardsWrapper}>
         {restaurants &&
-          restaurants.map((restaurant) => (
+          filteredRestaurants.map((restaurant) => (
             <Card key={restaurant.id} restaurant={restaurant} />
           ))}
       </div>
